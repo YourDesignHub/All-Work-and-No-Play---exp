83258fcd8b9e40b5862b68a139ab8a71efdd7f60
Pixel\Chris.Godber <chrispgodber@gmail.com>
2017-05-08 16:57:30 +0100

    wokring on adding main functions onlclicks etc *Not Finished

--- app/resources/build/components/App.js
+++ app/resources/build/components/App.js
@@ -44,48 +44,77 @@ export default class App extends PixelComponent {
 	onClickChunk(event) {
 		var textChunks = this.state.textChunks;
 		var currentChunk = event.target.getAttribute('value');
-
 		console.log(currentChunk);
 	}
 
-	onClickRandomise(evt) {
+	onClickRefresh(event) {
 		var textChunks = this.state.textChunks;
 		var randomise_Values = _.sample(textChunks);
 
-		for (i = 0; i < textChunks.length; i++) {
-			randomise_Values += textChunks[i];
-			textChunks.splice(textChunks.length, 0, randomise_Values);
-		}
-
 		this.setState()
 		{
-			textChunks: textChunks
+			randText: randText
 		}
 
 	}
 
+	onClickClear( event){
+		var textChunks = [
+		{ id: 1, text: 'All' },
+				{ id: 2, text: 'Work' },
+				{ id: 3, text: 'No Play' },
+				{ id: 4, text: 'Make Jack' },
+				{ id: 5, text: 'a Dull boy' }
+		]
 
+	}
+
+	returnRandText() {
+		var textChunksNo = textChunks.length;
+		var randText = _.sample(textChunks);
+		console.log(randText);
+
+		this.setState()
+		{
+			randText: randText
+		}
+
+	}
+
+	rowChange (event){
+		// var ArrRowClone = _.clone(rows);
+		var rowNum = event.target.getAttribute('data-value');
+		// var newRows = _.slice(ArrRowClone,0,rowNum.length);
+		console.log('new rows', rowNum);
+		// console.log('new rows', newRows);
+
+		// this.setState()
+		// {
+		// 	newRows: rows
+		// }
+	}
 
 	render() {
 		var slices = this.state.slices;
 		var rows = this.state.rows;
 		var textChunks = this.state.textChunks;
-		var cloneChunks = _.clone(textChunks);
-		var randomise_Values = _.sample(textChunks);
-		var randText = this.state.randText;
 
+		var randText = _.sample(textChunks);
+
+		console.log(textChunks);
 		console.log(randText);
 
 		return (
 			<div className={AppStyles.container}>
 				<h1>All Work and No Play</h1>
+				<h2>Jacks Current Mood: {randText.text}</h2>
 				{this.state.rows.map((rows, index) => (
 					<div className={bootstrap.row}>
 						{this.state.slices.map((slices) => (
 							<div key={slices.id} className={AppStyles.imgSlice}>
 								{this.state.textChunks.map((textChunks) => (
 									<div key={textChunks.id} onClick={this.onClickChunk}>
-										<p value={randomise_Values.text}>{randomise_Values.text}</p>
+										<p value={randText.text}>{randText.text}</p>
 									</div>
 								))}
 							</div>
@@ -93,19 +122,19 @@ export default class App extends PixelComponent {
 					</div>
 				))}
 				<div className={bootstrap.row}>
-					<button className={AppStyles.button} onClick={this.onClickRandomise}>Randomise</button>
-					<button className={AppStyles.button} onClick={this.onClickRandomise}>Clear Board</button>
+					<button className={AppStyles.button} onClick={this.onClickRefresh}>Refresh</button>
+					<button className={AppStyles.button} onClick={this.onClickClear}>Clear Board</button>
 				</div>
 
 				<div className={bootstrap.row}>
 					<label><h3>Rows:</h3>
-						<input type="text" className={AppStyles.input} value={this.state.rows.length} onChange={this.rowChange} />
+						<input type="text" className={AppStyles.input} data-value ={this.state.rows.length} value={this.state.rows.length} onChange={this.rowChange} />
 					</label>
 					<label><h3>Slices:</h3>
 						<input type="text" className={AppStyles.input} value={this.state.slices.length} onChange={this.sliceChange} />
 					</label>
 				</div>
-				
+
 			</div>
 		)
 	}
